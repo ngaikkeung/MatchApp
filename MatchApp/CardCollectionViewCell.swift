733ab8688db122b232b3d7cfa2ec2802d27d2b6e
ngaikkeung <ngaikkeung@gmail.com>
2020-06-20 02:54:21 +0800

    Update: add card flip up and down animation

--- MatchApp/CardCollectionViewCell.swift
+++ MatchApp/CardCollectionViewCell.swift
@@ -14,4 +14,40 @@ class CardCollectionViewCell: UICollectionViewCell {
     
     @IBOutlet weak var backImageView: UIImageView!
     
+    var card: Card?
+    
+    func configureCell(card: Card){
+        
+        // Assign card
+        self.card = card
+        
+        // Set the front image view to the associated card
+        frontImageView.image = UIImage(named: card.imageName)
+        
+        // Reset the stat of the card by card prop and show front and back correctly
+        if card.isFlipped == true{
+            // Show front image view
+            flipUp(speed: 0)
+        }else {
+            // Show back image view
+            flipDown(speed: 0)
+        }
+        
+    }
+    
+    func flipUp(speed: TimeInterval = 0.3){
+        // Transition to flip up the selected card
+        UIView.transition(from: backImageView, to: frontImageView, duration: speed, options: [.showHideTransitionViews, .transitionFlipFromLeft], completion: nil)
+        
+        // Update the state of card
+        card?.isFlipped = true
+    }
+    
+    func flipDown(speed: TimeInterval = 0.3){
+        // Transition to flip down the selected card
+        UIView.transition(from: frontImageView, to: backImageView, duration: speed, options: [.showHideTransitionViews, .transitionFlipFromRight], completion: nil)
+        
+        // Update the state of card
+       card?.isFlipped = false
+    }
 }
--- MatchApp/ViewController.swift
+++ MatchApp/ViewController.swift
@@ -20,14 +20,14 @@ class ViewController: UIViewController, UICollectionViewDataSource, UICollection
         // Do any additional setup after loading the view.
         
         cards = model.getCards()
-        print(cards.count)
+        
         // Set the view controller as the data source and delegate of the collection view
         collectionView.dataSource = self
         collectionView.delegate = self
       
     }
 
-    // MARK: - Collection View delegate method
+    // MARK: - Collection View data source method
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return cards.count
     }
@@ -35,14 +35,27 @@ class ViewController: UIViewController, UICollectionViewDataSource, UICollection
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         
         // Get a cell
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "CardCell", for: indexPath)
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "CardCell", for: indexPath) as! CardCollectionViewCell
         
         // TODO: Configure it
+        cell.configureCell(card: cards[indexPath.row])
         
         // Return it
         return cell
     }
     
-
+    // MARK: - Collection View delegate method
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        
+        // Get a reference to the cell was selected
+        let cell = collectionView.cellForItem(at: indexPath) as? CardCollectionViewCell
+        
+        // Check the state of the card to determine flip up or down
+        if(cell?.card?.isFlipped == false){
+            cell?.flipUp()
+        }else{
+            cell?.flipDown()
+        }
+    }
 }
 
